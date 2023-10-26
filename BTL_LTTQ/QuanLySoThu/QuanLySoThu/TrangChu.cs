58af3d6441530e4548c3d98cfa24a5c50eaad725
DungNoCopy11111 <104315226+DungNoCopy11111@users.noreply.github.com>
2023-10-26 21:42:39 +0700

    Add files via upload

---
+++ BTL_LTTQ/QuanLySoThu/QuanLySoThu/TrangChu.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySoThu
+{
+    public partial class frmTrangChu : Form
+    {
+        DataBaseProcess database = new DataBaseProcess();
+        public frmTrangChu()
+        {
+            InitializeComponent();
+        }
+        private static string userName = "";
+        public static string UserName { get => userName; set => userName = value; }
+
+        private void frmTrangChu_Load(object sender, EventArgs e)
+        {
+            lblUser.Text = UserName;
+        }
+
+        private void btnThu_Click(object sender, EventArgs e)
+        {
+            pnFooter.Visible = true;
+            pnMain.Controls.Clear();
+
+            pageThu pagethu = new pageThu();
+            pagethu.Dock = DockStyle.Fill;
+            pnMain.Controls.Add(pagethu);
+
+            //lblTongBanGhi.Text = GetNumberOfRecords("select count(mathu) from thu");
+        }
+
+        private void btnNhanSu_Click(object sender, EventArgs e)
+        {
+            pnFooter.Visible = true;
+            pnMain.Controls.Clear();
+
+            pageNhanSu pagens = new pageNhanSu();
+            pagens.Dock = DockStyle.Fill;
+            pnMain.Controls.Add(pagens);
+        }
+
+        private void btnChuong_Click(object sender, EventArgs e)
+        {
+            pnFooter.Visible = true;
+            pnMain.Controls.Clear();
+
+            pageChuong pagechuong = new pageChuong();
+            pagechuong.Dock = DockStyle.Fill;
+            pnMain.Controls.Add(pagechuong);
+        }
+
+        private void btnBaoCao_Click(object sender, EventArgs e)
+        {
+            pnFooter.Visible = true;
+            pnMain.Controls.Clear();
+
+            pageBaoCao pagebc = new pageBaoCao();
+            pagebc.Dock = DockStyle.Fill;
+            pnMain.Controls.Add(pagebc);
+        }
+
+        private void btnNhapLieu_Click(object sender, EventArgs e)
+        {
+            pnFooter.Visible = true;
+            pnMain.Controls.Clear();
+
+            pageNhapLieu pagenl = new pageNhapLieu();
+            pagenl.Dock = DockStyle.Fill;
+            pnMain.Controls.Add(pagenl);
+        }
+    }
+}
